099d9a4881102bdceac3c183f3a5776c90c83f9f
1amanton <iamantongit@gmail.com>
2022-02-15 20:19:51 +0300

    add intro component html/sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Intro from "./components/intro/Intro";
 import "./styles/style.css";
 
 function App() {
   return (
     <div className="App">
-      Portfolio App
+      <Intro/>
     </div>
   );
 }
